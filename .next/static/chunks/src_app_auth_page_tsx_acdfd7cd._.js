9b874ba81ca4d71659429f53eeb3724ca7cb011a
gustavovre <gustavobaraopj@gmail.com>
2025-07-25 21:37:33 -0300

    Add .next/static/chunks/src_app_auth_page_tsx_acdfd7cd._.js 1753490252677

---
+++ .next/static/chunks/src_app_auth_page_tsx_acdfd7cd._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@supabase_node-fetch_browser_5ac0ac0f.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_@supabase_auth-js_dist_module_6004bf20._.js",
+  "static/chunks/node_modules_zod_7a42b4f2._.js",
+  "static/chunks/node_modules_9ced490e._.js",
+  "static/chunks/src_906230c4._.js"
+],
+    source: "dynamic"
+});
