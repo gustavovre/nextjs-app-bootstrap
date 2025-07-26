55add28b85aaff0082b6fec30aef66b90c50617c
gustavovre <gustavobaraopj@gmail.com>
2025-07-25 21:37:44 -0300

    Add .next/static/chunks/src_app_page_tsx_acdfd7cd._.js 1753490263322

---
+++ .next/static/chunks/src_app_page_tsx_acdfd7cd._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_022e1369._.js",
+  "static/chunks/src_c739a045._.js"
+],
+    source: "dynamic"
+});
